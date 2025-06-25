5be53c75773561952c879225edc33797d896a431
Charlie O'Toole <charlieotoole98@gmail.com>
2025-06-25 11:09:02 +0100

    Fix register route: use site field instead of username and update register template

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or 'fallbacksecret'
 
 # --- Database Configuration ---
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_NEW')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
